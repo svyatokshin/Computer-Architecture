b1203757c208205467a1933356738fc1747b6725
svyatoslav-okshin <svyatokshin@gmail.com>
2020-10-09 13:37:26 -0400

    day 1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.pc = 0
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +21,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +50,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +62,33 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.reg[address] = value
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running:
+            command = self.ram[self.pc]
+            self.trace()
+            if command == 0b100000010:
+                self.reg[self.ram_read(self.pc + 1)
+                         ] = self.ram_read(self.pc + 2)
+                self.pc += 2
+
+            elif command == 0b01000111:
+                print(self.reg[self.ram_read(self.pc + 1)])
+                self.pc += 1
+
+            elif command == 0b00000001:
+                running = False
+
+            else:
+                print('unknown command!')
+                running = False
+
+            self.pc += 1
